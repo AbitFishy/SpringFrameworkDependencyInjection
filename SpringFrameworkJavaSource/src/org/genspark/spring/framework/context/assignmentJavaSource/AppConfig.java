001e2a8c96ee50ba8e1ec9b5aca5a35f0e5ac4fa
Robert C. Kowalczyk <catdogramb@gmail.com>
2022-05-16 16:16:52 -0400

    This is so confusing...what is happening?

--- SpringFrameworkJavaSource/src/org/genspark/spring/framework/context/assignmentJavaSource/AppConfig.java
+++ SpringFrameworkJavaSource/src/org/genspark/spring/framework/context/assignmentJavaSource/AppConfig.java
@@ -10,22 +10,31 @@ import java.util.List;
 @Configuration
 public class AppConfig {
 
-    @Bean
+    @Bean("student")
     public Student getStudent(List<Phone> ph, Address address){
         return new Student(548652, "John Doe", ph, address);
     }
 
     @Bean("ph")
     List<Phone> listPhones(){
+        System.out.println("In ListPhones()");
         List<Phone> phones = new ArrayList<>();
-        phones.add(new Phone("123-456-7890"));
+        phones.add(new Phone());
+        phones.add(new Phone("548-985-9855"));
         return phones;
     }
 
     @Bean("phone")
     public Phone getPhone(){
-        Phone p = new Phone();
+        System.out.println("In getPhone()");
+        Phone p = new Phone("123-456-7890");
         return p;
+    }
+
+    @Bean("phone")
+    public Phone getPhone2(String number){
+        System.out.println("In getPhone2()");
+        return new Phone(number);
     }
 
     @Bean("address")
